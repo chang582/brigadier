f9873333a94916860b53223f2cae7e6e98c26bf3
Joshua Chang <joshua@hiwaldo.com>
2026-01-28 17:56:06 -0800

    python3 compatibility

--- build_windows_exe.py
+++ build_windows_exe.py
@@ -1,80 +1,109 @@
+#!/usr/bin/env python3
+#
 # build_windows_exe.py
 #
 # Builds a Windows exe of brigadier using PyInstaller. PyInstaller
 # will be downloaded to the cwd and its archive kept for later use.
 #
 # Requires:
-# - Python 2.7 for Windows (http://www.python.org/getit)
-# - pywin32 (http://sourceforge.net/projects/pywin32)
-#
-# Note (2022-11-07): This script needs to be overhauled to instead
-# just use a virtualenv. PyInstaller 3.6 needs pywin32-ctypes, and
-# also to pass a .spec file into pyinstaller, so that the exe contains
-# more metadata and version info. As-is the script doesn't currently
-# work, and I've just bumped the PyInstaller version to 3.6 as a reminder
-# that this is the last version that is still compatible with Python 2.7.
+# - Python 3.8+
+# - Run this script on Windows to build the exe locally.
+#   On other platforms, use CI (e.g. GitHub Actions) to build on Windows.
 
-import urllib
-import os
-from zipfile import ZipFile, BadZipfile
-import subprocess
-import shutil
 import hashlib
+import os
+import platform
+import shutil
+import subprocess
+import sys
+from zipfile import BadZipFile, ZipFile
 
-PYINSTALLER_URL = 'https://github.com/pyinstaller/pyinstaller/archive/refs/tags/v3.6.zip'
-PYINST_ZIPFILE = os.path.join(os.getcwd(), 'pyinstaller.zip')
-NAME = 'brigadier'
+# PyInstaller 6.x supports Python 3.8+
+PYINSTALLER_VERSION = "6.10.0"
+PYINSTALLER_URL = (
+    f"https://github.com/pyinstaller/pyinstaller/archive/refs/tags/v{PYINSTALLER_VERSION}.zip"
+)
+PYINST_ZIPFILE = os.path.join(os.getcwd(), "pyinstaller.zip")
+NAME = "brigadier"
 
-with open('VERSION', 'r') as fd:
-    version = fd.read().strip()
-name_versioned = NAME + '-' + version
-exe_name = NAME + '.exe'
-pack_dir = os.path.join(os.getcwd(), name_versioned)
-build_dir = os.path.join(os.getcwd(), 'build')
 
-need_pyinstaller = False
-if os.path.exists(PYINST_ZIPFILE):
-    print "PyInstaller zipfile found."
-    try:
-        ZipFile(PYINST_ZIPFILE)
-    except BadZipfile:
-        print "Zipfile is corrupt."
+def main():
+    if platform.system() != "Windows":
+        print("This script builds a Windows exe and must be run on Windows.")
+        print("On Mac/Linux, push to GitHub and use Actions to build on Windows.")
+        sys.exit(1)
+
+    with open("VERSION", "r") as fd:
+        version = fd.read().strip()
+    name_versioned = NAME + "-" + version
+    exe_name = NAME + ".exe"
+    pack_dir = os.path.join(os.getcwd(), name_versioned)
+    build_dir = os.path.join(os.getcwd(), "build")
+
+    # Download PyInstaller if needed
+    need_pyinstaller = False
+    if os.path.exists(PYINST_ZIPFILE):
+        print("PyInstaller zipfile found.")
+        try:
+            with ZipFile(PYINST_ZIPFILE) as zf:
+                pass
+        except BadZipFile:
+            print("Zipfile is corrupt.")
+            need_pyinstaller = True
+    else:
         need_pyinstaller = True
-else:
-    need_pyinstaller = True
 
-if need_pyinstaller:
-    print "Downloading PyInstaller..."
-    urllib.urlretrieve(PYINSTALLER_URL, filename=PYINST_ZIPFILE)
+    if need_pyinstaller:
+        print("Downloading PyInstaller...")
+        import urllib.request
 
-dlzip = ZipFile(PYINST_ZIPFILE)
-pyinst_root = dlzip.namelist()[0].split("/")[0]
-dlzip.extractall()
+        urllib.request.urlretrieve(PYINSTALLER_URL, filename=PYINST_ZIPFILE)
 
-print "Building version %s..." % version
-build_cmd = [os.path.join(os.environ["SYSTEMDRIVE"] + "\\", 'Python27', 'python.exe'),
-             os.path.join(os.getcwd(), pyinst_root, 'pyinstaller.py'),
-             '-F',
-             '--distpath', pack_dir,
-             '--name', NAME,
-             'brigadier']
-subprocess.call(build_cmd)
+    with ZipFile(PYINST_ZIPFILE) as dlzip:
+        pyinst_root = dlzip.namelist()[0].split("/")[0]
+    with ZipFile(PYINST_ZIPFILE) as dlzip:
+        dlzip.extractall()
 
-print "Compressing to zip file..."
-zipfile_name = name_versioned + '.zip'
-packzip = ZipFile(zipfile_name, 'w')
-packzip.write(os.path.basename(pack_dir))
-packzip.write(os.path.join(os.path.basename(pack_dir), exe_name))
-packzip.close()
-with open(zipfile_name, 'r') as zipfd:
-    sha1 = hashlib.sha1(zipfd.read()).hexdigest()
+    pyinst_root_path = os.path.join(os.getcwd(), pyinst_root)
+    if not os.path.isdir(pyinst_root_path):
+        sys.exit("PyInstaller root not found: " + pyinst_root_path)
 
-print "Cleaning up..."
-for dirs in [pack_dir, build_dir, pyinst_root]:
-    shutil.rmtree(dirs)
-for f in os.listdir(os.getcwd()):
-    if f.startswith('logdict'):
-        os.remove(f)
+    brigadier_script = os.path.join(os.getcwd(), "brigadier")
 
-print "Built and archived to %s." % zipfile_name
-print "SHA1: %s" % sha1
+    print("Building version %s..." % version)
+    build_cmd = [
+        sys.executable,
+        "-m",
+        "PyInstaller",
+        "-F",
+        "--distpath",
+        pack_dir,
+        "--name",
+        NAME,
+        brigadier_script,
+    ]
+    subprocess.check_call(build_cmd, cwd=pyinst_root_path)
+
+    print("Compressing to zip file...")
+    zipfile_name = name_versioned + ".zip"
+    exe_path = os.path.join(pack_dir, exe_name)
+    with ZipFile(zipfile_name, "w") as packzip:
+        packzip.write(exe_path, os.path.join(name_versioned, exe_name))
+
+    with open(zipfile_name, "rb") as zipfd:
+        sha1 = hashlib.sha1(zipfd.read()).hexdigest()
+
+    print("Cleaning up...")
+    for dirs in [pack_dir, build_dir, pyinst_root]:
+        if os.path.isdir(dirs):
+            shutil.rmtree(dirs)
+    for f in os.listdir(os.getcwd()):
+        if f.startswith("logdict"):
+            os.remove(os.path.join(os.getcwd(), f))
+
+    print("Built and archived to %s." % zipfile_name)
+    print("SHA1: %s" % sha1)
+
+
+if __name__ == "__main__":
+    main()
