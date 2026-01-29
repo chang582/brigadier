9ceeb41fc549eceaa2eb0202032bee01a3b5ce39
Joshua Chang <joshua@hiwaldo.com>
2026-01-28 18:10:39 -0800

    CI: Python 3.12, PyInstaller 6, and Python 3 brigadier

--- ci/windows/ci-windows.sh
+++ ci/windows/ci-windows.sh
@@ -2,19 +2,11 @@
 
 set -eux -o pipefail
 
-PYTHON_VERSION=2.7.18
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
-script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd )"
-python="/c/hostedtoolcache/windows/Python/${PYTHON_VERSION}/x64/python"
-
-export PYTHONPATH="${python}"
-$python -V
-$python -m pip install -r "${script_dir}/requirements.txt"
-$python -m pip freeze
-
-# pip-installed exes will be installed here, so we'll put those at the front
-# of the PATH
-PATH="/c/hostedtoolcache/windows/Python/${PYTHON_VERSION}/x64/Scripts:$PATH"
+python -V
+python -m pip install -r "${script_dir}/requirements.txt"
+python -m pip freeze
 
 pyinstaller \
     --onefile \
